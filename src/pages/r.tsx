90bdb20b55885f3c663977c78ee0ebf82fd95813
errplane <110797323+errplanedev@users.noreply.github.com>
2024-02-04 13:10:38 -0800

    Y.M.C.A

--- src/pages/r.tsx
+++ src/pages/r.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
 import styles from '../styles/r.module.css';
 
 export default function R() {
+  if(typeof window !== 'undefined') {
+    new Audio('/rick.mp3').play()
+  }
+
   return (
     <div className={styles.main}>
       <div className={styles.img}></div>
