731cfc51395702b64f3e757135ee22c7b08bac02
errplane <110797323+errplanedev@users.noreply.github.com>
2024-02-10 08:56:51 -0800

    damn what is that grammer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <h2>an <span className="highlight">elon musk hater</span>.</h2>
         <h3 style={{ fontStyle: "italic" }}>there's currently not that much here, but you can <a href="/posts">check out my blog</a>.</h3>
         <br />
-        <h3>this website uses umami, if you want to see them go <a href="https://umami.dour.com/share/I6sQ8qyv8xpwMBJi/personal">here</a>.</h3>
+        <h3>this website uses umami for analytics (doesnt require cookies), if you want to see them go <a href="https://umami.dour.com/share/I6sQ8qyv8xpwMBJi/personal">here</a>.</h3>
       </>
     )
 }
