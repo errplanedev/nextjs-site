5dcf1522de10bb77b4fe66c1017ee229a182fc7c
errplane <110797323+errplanedev@users.noreply.github.com>
2024-02-10 08:54:13 -0800

    yes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,9 @@ export default function Home() {
         <h1>hi, i'm <span className="highlight">errplane</span>.</h1>
         <br />
         <h2>an <span className="highlight">elon musk hater</span>.</h2>
-        <br />
         <h3 style={{ fontStyle: "italic" }}>there's currently not that much here, but you can <a href="/posts">check out my blog</a>.</h3>
+        <br />
+        <h3>this website uses umami, if you want to see them go <a href="https://umami.dour.com/share/I6sQ8qyv8xpwMBJi/personal">here</a>.</h3>
       </>
     )
 }
